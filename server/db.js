b5fa05a24f6fab630f5bbf4fe595a86efda5a696
Lochan <110106642+Lochan7JK@users.noreply.github.com>
2026-05-14 20:20:31 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -18,6 +18,32 @@
 //   .catch(err => console.error(err));
 
 
+
+// import pg from "pg";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const { Pool } = pg;
+
+// const db = new Pool({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT,
+// });
+
+// // No need for db.connect() with Pool, it manages connections automatically
+// db.on("connect", () => {
+//   console.log("PostgreSQL Connected 🚀");
+// });
+// // ) library, this line sets up an event listener that triggers every time 
+// // a new database client connection is successfully established and added to the connection pool.
+
+// export default db;
+
+
 import pg from "pg";
 import dotenv from "dotenv";
 
@@ -26,18 +52,14 @@ dotenv.config();
 const { Pool } = pg;
 
 const db = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-// No need for db.connect() with Pool, it manages connections automatically
 db.on("connect", () => {
   console.log("PostgreSQL Connected 🚀");
 });
-// ) library, this line sets up an event listener that triggers every time 
-// a new database client connection is successfully established and added to the connection pool.
 
 export default db;
