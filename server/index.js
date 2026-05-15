2ebcc0738239bf53403140d72b5cf6d8ff43a802
Lochan <110106642+Lochan7JK@users.noreply.github.com>
2026-05-15 18:06:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend-name.onrender.com",
+      "https://portfolio-with-dashboard-frontend.onrender.com",
     ],
     credentials: true,
   })
