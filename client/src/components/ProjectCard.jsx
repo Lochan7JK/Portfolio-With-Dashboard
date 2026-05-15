4bb18c38c59ef9aaddf9376fb78bedb6aee550da
Lochan <110106642+Lochan7JK@users.noreply.github.com>
2026-05-15 10:43:17 +0530

    Update ProjectCard.jsx

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -49,12 +49,12 @@ function ProjectCard({ img, title, description, live }) {
         </h5>
 
         <p className="text-gray-300 text-sm mb-3">
-          Built with logic & caffeine ☕
-          {/* {description} */}
+          {/* Built with logic & caffeine ☕ */}
+          {description}
         </p>
 
         <a href={live} target="_blank" rel="noopener noreferrer">
-          <button className="bg-[#00ADB5] px-4 py-2 rounded-md text-white hover:scale-105 transition">
+          <button className="bg-primary px-4 py-2 rounded-md text-white hover:scale-105 transition">
             View Project
           </button>
         </a>
