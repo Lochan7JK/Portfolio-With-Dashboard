4b937123e9c8d3613b84a692f57d1f8360168ca2
Lochan <110106642+Lochan7JK@users.noreply.github.com>
2026-05-15 10:45:55 +0530

    Change API URL to use environment variable
    Updated API URL to use environment variable for better configuration.

--- client/src/components/dashboard/AnalyticsTab.jsx
+++ client/src/components/dashboard/AnalyticsTab.jsx
@@ -18,7 +18,8 @@ function AnalyticsTab() {
       try {
 
         const res = await axios.get(
-          "http://localhost:5000/analytics"
+        //   "http://localhost:5000/analytics",
+            `${import.meta.env.VITE_API_URL}/analytics`,
         );
 
         setAnalytics(res.data);
