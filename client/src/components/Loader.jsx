7a9cbbd83b40692e9e44c3eee1503b338f819ede
Lochan <110106642+Lochan7JK@users.noreply.github.com>
2026-05-16 12:39:36 +0530

    Update Loader.jsx

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,49 +1,4 @@
-// import { useEffect, useState } from "react";
-
-// function Loader({ onFinish }) {
-//   const [text, setText] = useState("");
-// //   const fullText = "Initializing Experience...";
-//     const fullText = "Welcome, human. Initializing Lochan...";
-
-//   useEffect(() => {
-//     let i = 0;
-
-//     const typing = setInterval(() => {
-//       setText(fullText.slice(0, i));
-//       i++;
-
-//       if (i > fullText.length) {
-//         clearInterval(typing);
-
-//         setTimeout(() => {
-//           onFinish();
-//         }, 800);
-//       }
-//     }, 50);
-
-//     return () => clearInterval(typing);
-//   }, []);
-
-//   return (
-//     <div className="fixed inset-0 z-[999] bg-[#0B0F19] flex flex-col items-center justify-center text-white">
-      
-//       {/* Glow orb */}
-//       <div className="w-24 h-24 rounded-full bg-[#00ADB5] blur-3xl opacity-40 animate-pulse absolute"></div>
-
-//       {/* AI rotating ring */}
-//       <div className="w-20 h-20 border-2 border-t-[#00ADB5] border-white/10 rounded-full animate-spin mb-6"></div>
-
-//       {/* Typing text */}
-//       <p className="text-lg tracking-widest text-gray-300">
-//         {text}
-//         <span className="animate-pulse">|</span>
-//       </p>
-//     </div>
-//   );
-// }
-
-// export default Loader;
-
+// src/components/Loader.jsx
 
 import { motion } from "framer-motion";
 
@@ -54,7 +9,7 @@ function Loader({ text }) {
       animate={{ opacity: 1 }}
       exit={{ 
         opacity: 0,
-        scale: 1.2,   // 🔥 expands like morph
+        scale: 1.2,   // expands like morph
         transition: { duration: 0.8, ease: "easeInOut" }
       }}
       className="fixed inset-0 z-[999] bg-[#191919] flex items-center justify-center"
