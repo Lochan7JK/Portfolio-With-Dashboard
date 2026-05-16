3c6b2e060dbe7a5883edd29aad81b6aa0a26bd8a
Lochan <110106642+Lochan7JK@users.noreply.github.com>
2026-05-16 12:53:42 +0530

    Update ProtectedRoute.jsx

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -2,31 +2,6 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
-// export default function ProtectedRoute({ children }) {
-//   const { user } = useContext(AuthContext);
-
-//   if (!user) {
-//     return <Navigate to="/login" />;
-//   }
-
-//   return children;
-// }
-
-
-// export default function ProtectedRoute({ children }) {
-//   const { user, loading } = useContext(AuthContext);
-
-//   if (loading) {
-//     return <p className="text-white">Loading...</p>;
-//   }
-
-//   if (!user) {
-//     return <Navigate to="/login" />;
-//   }
-
-//   return children;
-// }
-
 
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useContext(AuthContext);
