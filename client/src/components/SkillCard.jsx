1e0438974be09c0039df209eb4464cc148b26da8
Lochan <110106642+Lochan7JK@users.noreply.github.com>
2026-05-16 12:57:38 +0530

    Update SkillCard.jsx

--- client/src/components/SkillCard.jsx
+++ client/src/components/SkillCard.jsx
@@ -4,46 +4,6 @@ import { motion } from "framer-motion";
 
 function SkillCard({ image, name, delay }) {
   return (
-    // <motion.div
-    //   variants={{
-    //     hidden: { opacity: 0, y: 40, scale: 0.9 },
-    //     visible: {
-    //       opacity: 1,
-    //       y: 0,
-    //       scale: 1,
-    //       transition: { duration: 0.4, ease: "easeOut" },
-    //     },
-    //   }}
-
-    //   // whileHover={{ scale: 1.1 }}
-    //   whileHover={{ scale: 1.1, boxShadow: "0px 0px 15px #00ADB5" }}
-
-    //   className="h-[90px] w-full rounded-md border-2 border-transparent 
-    //              bg-gradient-to-r from-[#EEEEEE] via-[#00ADB5] to-[#393E46] 
-    //              p-[2px] transition duration-300"
-    // >
-
-    // <motion.div
-    //   variants={{
-    //     hidden: { opacity: 0, scale: 0.92 },
-    //     visible: {
-    //       opacity: 1,
-    //       scale: 1,
-    //       transition: {
-    //         duration: 0.5,
-    //         ease: [0.25, 0.1, 0.25, 1], // smooth cubic-bezier
-    //       },
-    //     },
-    //   }}
-    //   whileHover={{
-    //     scale: 1.08,
-    //     boxShadow: "0px 0px 15px #00ADB5",
-    //   }}
-    //   className="h-[90px] w-full rounded-md border-2 border-transparent 
-    //             bg-gradient-to-r from-[#EEEEEE] via-[#00ADB5] to-[#393E46] 
-    //             p-[2px] transition duration-300"
-    // >
-
      <motion.div
         initial={{ opacity: 0, scale: 0.92 }}
         animate={{ opacity: 1, scale: 1 }}
