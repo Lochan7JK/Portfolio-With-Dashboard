404440e283e170504434b5747c66391973cd80cb
Lochan <110106642+Lochan7JK@users.noreply.github.com>
2026-05-16 12:26:13 +0530

    Update CursorGlow.jsx

--- client/src/components/CursorGlow.jsx
+++ client/src/components/CursorGlow.jsx
@@ -1,36 +1,4 @@
-// import { useEffect, useState } from "react";
-
-// function CursorGlow() {
-//   const [position, setPosition] = useState({ x: 0, y: 0 });
-
-//   useEffect(() => {
-//     const move = (e) => {
-//       setPosition({ x: e.clientX, y: e.clientY });
-//     };
-
-//     window.addEventListener("mousemove", move);
-
-//     return () => window.removeEventListener("mousemove", move);
-//   }, []);
-
-//   return (
-//     <div
-//       className="pointer-events-none fixed z-10 hidden md:block"
-//       style={{
-//         left: position.x,
-//         top: position.y,
-//         transform: "translate(-50%, -50%)",
-//       }}
-//     >
-//       <div className="w-40 h-40 bg-[#00ADB5] opacity-20 rounded-full blur-3xl" />
-//     </div>
-//   );
-// }
-
-// export default CursorGlow;
-
-
-
+// src/components/CursorGlow.jsx
 
 import { useEffect, useState } from "react";
 
@@ -88,51 +56,3 @@ function CursorGlow() {
 }
 
 export default CursorGlow;
-
-
-
-
-
-
-// import { useEffect, useState } from "react";
-
-// function CursorGlow() {
-//   const [position, setPosition] = useState({ x: 0, y: 0 });
-//   const [smooth, setSmooth] = useState({ x: 0, y: 0 });
-
-//   useEffect(() => {
-//     const move = (e) => {
-//       setPosition({ x: e.clientX, y: e.clientY });
-//     };
-
-//     window.addEventListener("mousemove", move);
-//     return () => window.removeEventListener("mousemove", move);
-//   }, []);
-
-//   useEffect(() => {
-//     const animate = () => {
-//       setSmooth((prev) => ({
-//         x: prev.x + (position.x - prev.x) * 0.1,
-//         y: prev.y + (position.y - prev.y) * 0.1,
-//       }));
-//       requestAnimationFrame(animate);
-//     };
-
-//     animate();
-//   }, [position]);
-
-//   return (
-//     <div
-//       className="pointer-events-none fixed z-10 hidden md:block"
-//       style={{
-//         left: smooth.x,
-//         top: smooth.y,
-//         transform: "translate(-50%, -50%)",
-//       }}
-//     >
-//       <div className="w-40 h-40 bg-[#00ADB5] opacity-20 rounded-full blur-3xl" />
-//     </div>
-//   );
-// }
-
-// export default CursorGlow;
