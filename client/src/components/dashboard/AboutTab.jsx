1352f1651dd53b21807db7affead04c4efc15bf7
Lochan <110106642+Lochan7JK@users.noreply.github.com>
2026-05-16 12:05:20 +0530

    Update AboutTab.jsx

--- client/src/components/dashboard/AboutTab.jsx
+++ client/src/components/dashboard/AboutTab.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 // import api from "../utils/apiToken";
-// const res = await api.get("/projects");
+// const res = await api.get("/about");
 
 function AboutTab() {
 
