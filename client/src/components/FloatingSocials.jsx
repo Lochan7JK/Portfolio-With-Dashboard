d4aa8e7cf0f2aad878305d153f7cba63b60464ed
Lochan <110106642+Lochan7JK@users.noreply.github.com>
2026-05-16 12:27:18 +0530

    Update FloatingSocials.jsx

--- client/src/components/FloatingSocials.jsx
+++ client/src/components/FloatingSocials.jsx
@@ -1,5 +1,6 @@
 
-// import { useEffect, useState } from "react";
+// src/components/FloatingSocials.jsx
+
 import { FaLinkedinIn, FaTwitter, FaInstagram, FaEnvelope, FaGithub} from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { useEffect, useState } from "react";
@@ -37,49 +38,9 @@ function FloatingSocials() {
     email: <MdEmail />,
     github: <FaGithub />,
   };
-
-//   const [isMobile, setIsMobile] = useState(false);
-
-//   useEffect(() => {
-//     const check = () => setIsMobile(window.innerWidth < 1100);
-//     check();
-//     window.addEventListener("resize", check);
-
-//     return () => window.removeEventListener("resize", check);
-//   }, []);
-
-//   if (isMobile) return null; // ✅ hide on small screens
+  
 
   return (
-    // <div className="fixed right-6 top-1/2 -translate-y-1/2 z-40 flex flex-col gap-6 text-gray-400 ">
-    
-    // <div className="hidden lg:flex fixed right-6 top-1/2 -translate-y-1/2 z-40 flex-col gap-6 text-gray-400">
-
-    //   <a href="https://www.linkedin.com/in/lochan-singoria-503347243/" target="_blank">
-    //     <FaLinkedinIn className="text-2xl hover:text-white hover:scale-125 transition duration-300 hover:drop-shadow-[0_0_10px_#00ADB5]" />
-    //   </a>
-
-    //   <a href="https://x.com/LochanSing13646" target="_blank">
-    //     <FaTwitter className="text-2xl hover:text-white hover:scale-125 transition duration-300 hover:drop-shadow-[0_0_10px_#00ADB5]" />
-    //   </a>
-
-    //   <a href="#" target="_blank">
-    //     <FaInstagram className="text-2xl hover:text-white hover:scale-125 transition duration-300 hover:drop-shadow-[0_0_10px_#00ADB5]" />
-    //   </a>
-
-    //   <a href="mailto:lochansingoria@gmail.com?subject=Let's%20Work%20Together" target="_blank">
-    //     <MdEmail className="text-2xl hover:text-white hover:scale-125 transition duration-300 hover:drop-shadow-[0_0_10px_#00ADB5]" />
-    //     {/* <FaEnvelope className="text-2xl hover:text-white hover:scale-125 transition duration-300 hover:drop-shadow-[0_0_10px_#00ADB5]" /> */}
-    //   </a>
-    //   {/* if mailto doesn't work this will always work instead of mailto: onClick={() => window.open("https://mail.google.com/mail/?view=cm&fs=1&to=lochansingoria@gmail.com")} */}
-
-    //   <a href="https://github.com/Lochan7JK" target="_blank">
-    //     <FaGithub className="text-2xl hover:text-white hover:scale-125 transition duration-300 hover:drop-shadow-[0_0_10px_#00ADB5]" />
-    //   </a>
-// 
-    // </div>
-
-
     <div className="hidden lg:flex fixed right-6 top-1/2 -translate-y-1/2 z-40 flex-col gap-6 text-gray-400">
 
       {socials
